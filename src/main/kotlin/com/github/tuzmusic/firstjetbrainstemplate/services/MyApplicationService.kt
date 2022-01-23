77dbcd43babc5203bfd53eae85b4ec44e77b8039
GitHub Action <action@github.com>
2022-01-23 15:19:19 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tuzmusic/firstjetbrainstemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tuzmusic.firstjetbrainstemplate.services
+
+import com.github.tuzmusic.firstjetbrainstemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
